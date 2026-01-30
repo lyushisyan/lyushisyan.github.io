2d9c59488258233ca0660a2c0b1506cb363fead8
lyushisyan <168818516+lyushisyan@users.noreply.github.com>
2026-01-30 21:21:25 +0000

    Deploying to gh-pages from @ lyushisyan/lyushisyan.github.io@10cc0e50e6cb19939ccd23ff1b8b233c0991748e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -261,30 +261,4 @@ ninja.data = [{
         handler: () => {
           window.open("https://orcid.org/0000-0003-3042-7817", "_blank");
         },
-      },{
-      id: 'light-theme',
-      title: 'Change theme to light',
-      description: 'Change the theme of the site to Light',
-      section: 'Theme',
-      handler: () => {
-        setThemeSetting("light");
-      },
-    },
-    {
-      id: 'dark-theme',
-      title: 'Change theme to dark',
-      description: 'Change the theme of the site to Dark',
-      section: 'Theme',
-      handler: () => {
-        setThemeSetting("dark");
-      },
-    },
-    {
-      id: 'system-theme',
-      title: 'Use system default theme',
-      description: 'Change the theme of the site to System Default',
-      section: 'Theme',
-      handler: () => {
-        setThemeSetting("system");
-      },
-    },];
+      },];
